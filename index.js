eb14bda5ab8fccef9bd717fb006b63cd30a1df01
Simon Oliveira Sousa <simonsousa335@gmail.com>
2025-04-22 20:30:54 -0300

    Adicionando o arquivo index com 3 rotas

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express"); //importa o módulo express neste arquivo
+const app = express(); //iniciando o express
+
+//criando a rota inicial
+app.get("/", function(req,res){
+    res.send("<h1>Bem vindo ao meu site!</h1>");
+})
+
+//rota do cadastro de produtos
+app.get("/produtos", function(req,res){
+    res.send("<h1>Lista de Produtos!</h1>");
+})
+
+//rota com parametro 
+app.get("/consulta/:parametro", function(req,res){
+    //req --> dados enviados pelo cliente
+    //res --> resposta enviada pelo servidor de volta ao cliente
+    res.send("retorno consulta:" + req.params.parametro);
+})
+
+app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicação na porta 3000
+    if (erro){
+        console.log("Erro ao Iniciar.");
+    }else{
+        console.log("Servidor Iniciado.");
+    }
+})
+
