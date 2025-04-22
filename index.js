e26f348524a87a35a071759ddd97e92284e7a6c0
Abrahão Levy <abrahaolevyy@gmail.com>
2025-04-22 20:49:43 -0300

    alterando o arquivo index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express(); //iniciando o express
 
 //criando a rota inicial
 app.get("/", function(req,res){
-    res.send("<h1>Bem vindo ao meu site!</h1>");
+    res.send("<h1>Bem vindo ao meu site!</h1><p>Abaixo do título, está o parágrafo</p>");
 })
 
 //rota do cadastro de produtos
@@ -15,7 +15,7 @@ app.get("/produtos", function(req,res){
 app.get("/consulta/:parametro", function(req,res){
     //req --> dados enviados pelo cliente
     //res --> resposta enviada pelo servidor de volta ao cliente
-    res.send("retorno consulta:" + req.params.parametro);
+    res.send(`<h3>retorno consulta: ${req.params.parametro}</h3>`);
 })
 
 app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicação na porta 3000
